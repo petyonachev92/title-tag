0adf8e4ab75cc87a8efcbfe799f6559f39347338
Petyo <pyo@abv.bg>
2021-12-27 12:46:57 +0200

    feat: add title change

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,11 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  setTimeout(
+    () => {
+      document.getElementsByTagName('title')[0].textContent = 'One new message'
+    }, 3000
+  )
 });
+
+
